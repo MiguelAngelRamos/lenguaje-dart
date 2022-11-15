b7a920229e27449b2a0b5ed16d2ce16dda9dacc5
Miguel Ramos <return.infox@gmail.com>
2022-11-15 18:48:44 -0300

    tipos en dart

---
+++ 01_introduccion/1_types.dart
@@ -0,0 +1,26 @@
+void main(List<String> args) {
+  //* infiere el tipo una vez que lo identifica no puedo asignarle otro valor que no sea del tipo inferido
+  var numero = 20;
+  // numero = 'miguel';
+  print(numero);
+  // print("Hola Mundo");
+  //* null salfety
+  int? edad = null;
+  print(edad);
+  //* Strings
+  // var cuidad = 'Madrid';
+  String cuidad = 'Madrid';
+  print(cuidad); //* Madrid
+  String? pais;
+  print(pais); //* null 
+  //* 'Madrid' o "Madrid", da lo mismo, lo que quieras utilizar
+
+  //* Interpolación
+
+  String nickname = 'mramos';
+  String game = 'AgeOfEmpires';
+
+  print("el nick: $nickname, es un jugador de: $game");
+  //* el nick: mramos, es un jugador de: AgeOfEmpires
+
+}
